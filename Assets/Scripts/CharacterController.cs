b838ad5db0a0444b6069dcca4c74fd799ad400bf
ab0214 <ab0214@student.jamk.fi>
2022-05-05 15:25:24 +0300

    fixed x/z axis problem again

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -12,7 +12,7 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera flightVCam;
     [SerializeField] private CinemachineVirtualCamera driveVCam;
     private bool flightMode = true;
-    private float velocityY = 0;
+    private float velocityY = 20; // falls through ground if this is any lower ???
     private int lane = 0; // -1, 0, 1
     private float laneWidth = 2; // how much the character moves left/right
 
@@ -64,12 +64,13 @@ public class CharacterController : MonoBehaviour
         }
 
         // smoothly translate character between lanes
-        float xTime = 0.08f;                                        // amount of time (seconds) it takes to change lanes;
-        float xError = lane * laneWidth - transform.position.x;     // how far the character is form where it should be.
-        float xMax = laneWidth / xTime * Time.deltaTime;            // maximum translation amont in this frame
-        float xTranslateAmout = Mathf.Clamp(xError, -xMax, xMax);   // how much the character is translated in this frame. (Mathf.Clamp sets correct +/- sign to zMax and prevents overshooting)
+        float time = 0.08f;                                        // amount of time (seconds) it takes to change lanes;
+        float error = lane * laneWidth - transform.position.x;     // how far the character is form where it should be.
+        float maxMovement = laneWidth / time * Time.deltaTime;            // maximum translation amont in this frame
+        float translateAmount = Mathf.Clamp(error, -maxMovement, maxMovement);   // how much the character is translated in this frame. (Mathf.Clamp sets correct +/- sign to maxMovement and prevents overshooting)
 
         // move character
-        transform.Translate(xTranslateAmout, velocityY * Time.deltaTime, 0);
+        transform.Translate(translateAmount, velocityY * Time.deltaTime, 0);
+        Debug.Log(transform.position.y + " " + velocityY);
     }
 }
