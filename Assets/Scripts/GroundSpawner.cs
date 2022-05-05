75b23f72a37ca9dcc62c595f1377792d206d5e1a
ab0214 <ab0214@student.jamk.fi>
2022-05-05 14:30:13 +0300

    added continuous infinite level generation to ground spawner script

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -6,18 +6,35 @@ public class GroundSpawner : MonoBehaviour
 
 {
     public GameObject groundTile;
-    Vector3 nextSpawnPoint;
+    GameObject lastTile;
+    GameObject player;
+    //Vector3 nextSpawnPoint;
 
     public void SpawnTile()
 	{
-        GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
-        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+        Vector3 nextSpawnPoint = lastTile.transform.GetChild(1).transform.position;
+        lastTile = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+        //nextSpawnPoint = lastTile.transform.GetChild(1).transform.position;
 	}
+
+    private void Update()
+    {
+        if (Vector3.Distance(lastTile.transform.position, player.transform.position) < 30)
+        {
+            SpawnTile();
+
+            GroundTile[] groundTiles = GameObject.FindObjectsOfType<GroundTile>();
+            GameObject firstTile = groundTiles[groundTiles.Length - 1].gameObject;
+            GameObject.Destroy(firstTile);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 15; i++)
+        player = GameObject.FindGameObjectWithTag("Player");
+        lastTile = Instantiate(groundTile, new Vector3(), Quaternion.identity);
+        for (int i = 0; i < 5; i++)
         {
             SpawnTile();
         }
