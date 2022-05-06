24da494040b740a49a145aa0199558a975a5985c
GWSun1027 <78084812+GWSun1027@users.noreply.github.com>
2022-05-06 16:39:57 +0300

    latest version

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
